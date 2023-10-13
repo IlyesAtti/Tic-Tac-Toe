96b3b49d3e3ab99e95aa189c38049f07ea43ff38
Ilyes Attila <attis.ilyes@gmail.com>
2023-10-13 19:37:55 +0300

    v1.2

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
-let ply = 0;
+let player = 0;
+let winerCase = ['1', '2','3', '4', '5', '6', '7', '8', '9', '1', '5', '9', '1', '4', '7', '2', '5', '8', '3', '6', '9', '3', '5', '7'];
+let xs =  [];
+let os =  [];
 
 function printTheCharacter(id) {
-    if (ply % 2 == 0) {
+    if (player % 2 == 0) {
         document.getElementById(id).innerText = 'X';
+        xs.push(Number(id));
     } else {
         document.getElementById(id).innerText = "O";
+        os.push(Number(id));  
     }
-    ++ply;
-    return (verifWiner(id)); 
+    ++player;
+    
+    return (verifWiner(id));
 }
 
 function verifWiner(id) {
-    let msg = 0;
+    xs.sort((a, b) => a - b);
+    os.sort((a, b) => a - b);
+    let mesages = 0;
     let winer = document.getElementById(id).innerText;
-    if(msg == 0) {
-        if (ply == 9) {
+    if (mesages == 0) {
+        if (player == 9) {
             document.getElementById('mesage').innerText = "Draw game";
         }
-        if (((document.getElementById(1).innerText != '') && (document.getElementById(1).innerText == document.getElementById(2).innerText) && (document.getElementById(2).innerText == document.getElementById(3).innerText))||
-            ((document.getElementById(4).innerText != '') && (document.getElementById(4).innerText == document.getElementById(5).innerText) && (document.getElementById(5).innerText == document.getElementById(6).innerText))||
-            ((document.getElementById(7).innerText != '') && (document.getElementById(7).innerText == document.getElementById(8).innerText) && (document.getElementById(8).innerText == document.getElementById(9).innerText))||
-            ((document.getElementById(1).innerText != '') && (document.getElementById(1).innerText == document.getElementById(5).innerText) && (document.getElementById(5).innerText == document.getElementById(9).innerText))||
-            ((document.getElementById(1).innerText != '') && (document.getElementById(1).innerText == document.getElementById(4).innerText) && (document.getElementById(4).innerText == document.getElementById(7).innerText))||
-            ((document.getElementById(2).innerText != '') && (document.getElementById(2).innerText == document.getElementById(5).innerText) && (document.getElementById(5).innerText == document.getElementById(8).innerText))||
-            ((document.getElementById(3).innerText != '') && (document.getElementById(3).innerText == document.getElementById(6).innerText) && (document.getElementById(6).innerText == document.getElementById(9).innerText))||
-            ((document.getElementById(3).innerText != '') && (document.getElementById(3).innerText == document.getElementById(5).innerText) && (document.getElementById(5).innerText == document.getElementById(7).innerText))) {
-            document.getElementById('mesage').innerText = "Player " + winer +" win";
-            msg = 1;
+        for (let i = 0; i < winerCase.length; i += 3) {
+            let curentPozition = i;
+            let founded = 0;
+            for (let j = 0; j < xs.length; ++j) {
+                if (winerCase[curentPozition] == xs[j]) {
+                    ++founded;
+                }
+                ++curentPozition;
+            }
+            if (founded >= 3) {
+                document.getElementById('mesage').innerText = "Player " + winer +" win";
+                mesages = 1;
+            }
+        }
+        for (let i = 0; i < winerCase.length; i += 3) {
+            let curentPozition = i;
+            let founded = 0;
+            for (let j = 0; j < os.length; ++j) {
+                if (winerCase[curentPozition] == os[j]) {
+                    ++founded;
+                }
+                ++curentPozition;
+            }
+            if (founded >= 3) {
+                document.getElementById('mesage').innerText = "Player " + winer +" win";
+                mesages = 1;
+            }
         }
     }
-    if (msg == 0 && ply == 9) {
+    if (mesages == 0 && player == 9) {
         document.getElementById('mesage').innerText = "Draw game";
     }
 }
